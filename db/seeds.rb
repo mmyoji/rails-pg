ed5913736d27651c37ce484943c29822508ed1e1
mmyoji <mmyoji@gmail.com>
2018-12-19 16:44:31 +0900

    User seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+[
+  { username: "test1", email: "test1@example.com" },
+  { username: "test2", email: "test2@example.com" },
+  { username: "test3", email: "test3@example.com" },
+].each do |attrs|
+  User.find_or_create_by(username: attrs.fetch(:username)) do |u|
+    u.email              = attrs.fetch(:email)
+    u.password_digest    = BCrypt::Password.create("password")
+    u.confirmation_token = SecureRandom.urlsafe_base64
+  end
+end
