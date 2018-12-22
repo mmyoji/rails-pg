61dba159ac3c727f72b8a4f90b03a3cd6bc5f357
mmyoji <mmyoji@gmail.com>
2018-12-23 08:08:12 +0900

    Create/Update Company in admin

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -11,21 +11,42 @@ class Admin::CompaniesController < Admin::BaseController
 
   # GET /companies/new
   def new
+    @company = Company.new
   end
 
   # POST /companies
   def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to admin_company_path(@company), notice: t("created")
+    else
+      render :new
+    end
   end
 
   # GET /companies/:id/edit
   def edit
+    @company = Company.find(params[:id])
   end
 
   # PATCH /companies/:id
   def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to admin_company_path(@company), notice: t("updated")
+    else
+      render :edit
+    end
   end
 
   # DELETE /companies/:id
   def destroy
+  end
+
+  private
+
+  def company_params
+    params.require(:company)
+      .permit(:name, :tel, :email, :address, :founded_at)
   end
 end
--- test/controllers/admin/companies_controller_test.rb
+++ test/controllers/admin/companies_controller_test.rb
@@ -22,13 +22,87 @@ class Admin::CompaniesControllerTest < ActionDispatch::IntegrationTest
     assert_response :not_found
   end
 
-  test "GET #new" do
+  test "GET #new returns 200" do
     get new_admin_company_url
     assert_response :success
   end
 
-  test "GET #edit" do
+  test "POST #create w/ valid params creates new Company" do
+    params = {
+      company: {
+        name: "New Company",
+        tel: "03-1111-1111",
+        email: "new-company@example.com",
+        address: "111-1111 Cyuoh, Tokyo",
+        founded_at: "2000-03-20",
+      }
+    }
+
+    assert_difference 'Company.count', 1 do
+      post admin_companies_path, params: params
+    end
+
+    new_company = Company.last
+    assert_equal params.dig(:company, :name), new_company.name
+    assert_equal params.dig(:company, :tel), new_company.tel
+    assert_equal params.dig(:company, :email), new_company.email
+    assert_equal params.dig(:company, :address), new_company.address
+    assert_equal params.dig(:company, :founded_at).to_date, new_company.founded_at
+
+    assert_redirected_to admin_company_path(new_company)
+  end
+
+  test "POST #create w/ invalid params doesn't create new Company" do
+    params = {
+      company: {
+        name: "New Company",
+        tel: "03-1111-1111",
+        email: "new-company@example.com",
+        address: "",
+        founded_at: "2000-03-20",
+      }
+    }
+
+    assert_no_difference 'Company.count' do
+      post admin_companies_path, params: params
+    end
+
+    assert_response :success
+  end
+
+  test "GET #edit returns 200" do
     get edit_admin_company_url(@company)
+    assert_response :success
+  end
+
+  test "GET #edit w/ invalid ID returns 404" do
+    get edit_admin_company_url(id: 1_000)
+    assert_response :not_found
+  end
+
+  test "PATCH #update w/ valid params changes Company data" do
+    params = {
+      company: {
+        name: "new-company-1",
+      },
+    }
+
+    patch admin_company_url(@company), params: params
+
+    assert_equal params.dig(:company, :name), @company.reload.name
+    assert_redirected_to admin_company_path(@company)
+  end
+
+  test "PATCH #update w/ invalid params doesn't change Company data" do
+    params = {
+      company: {
+        name: "",
+      },
+    }
+
+    patch admin_company_url(@company), params: params
+
+    refute_equal params.dig(:company, :name), @company.reload.name
     assert_response :success
   end
 end
